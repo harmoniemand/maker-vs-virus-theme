e741fc05687584926924ba7411924afaced08dcd
Jonathan Günz <jonathan.guenz@experimenta.science>
2020-03-28 16:28:07 +0100

    translations updated, added language switch in Menu

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,36 +1,29 @@
 <header>
-  <nav id="top-nav" class="bg-p vh-20 shadow p-4 navbar navbar-expand-lg text-white navbar-dark text-lg">
-    <a class="navbar-brand" href="#">
-		<img src="<?php echo get_template_directory_uri() ?>/images/logo.svg" width="95" height="33" />
-    </a>
-    <!-- MAIN NAV ITEMS FOR LATER
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-  -->
-	  
-    <div class="align-self-center ml-auto">
-      <ul class="navbar-nav">
-        <?php pll_the_languages(array('show_flags' => 1, 'show_names' => 1, 'dropdown' => 1)); ?>
-      </ul>
-    </div>
+    <nav id="top-nav" class="bg-p shadow p-4 navbar navbar-expand-lg text-white navbar-dark text-lg">
+        <a class="navbar-brand" href="#">
+            <img src="<?php echo get_template_directory_uri() ?>/images/logo.svg" width="95" height="33" />
+        </a>
 
-  </nav>
-	
+
+
+        <div class="align-self-center ml-auto">
+            <ul class="navbar-nav">
+
+                <?php $translations = pll_the_languages(array('raw' => 1)); ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php echo pll_current_language() ?>
+                    </a>
+                    <div class="dropdown-menu dropleft" aria-labelledby="navbarDropdown">
+                        <?php foreach ($translations as $translation) : ?>
+                            <a class="dropdown-item" href="<?php echo $translation['url'] ?>"><?php echo $translation['name'] ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+
+    </nav>
+
 </header>
